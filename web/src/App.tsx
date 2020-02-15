8c09948fe648f9954a1475c04a03171afaffc05c
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2020-02-15 15:18:32 +0700

    update homepage

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,32 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useLocation
+} from 'react-router-dom';
 import './App.css';
 
-const App: React.FC = () => {
+import { Grid, Avatar } from '@material-ui/core';
+import NotificationsNone from '@material-ui/icons/NotificationsNone';
+
+import { HomeView, NotificationView, ManageView, OverviewView, EmployeeView, TimetableView, WagesView } from './views/Views';
+
+const MenuItem = (props : any) => {
+  return <p className='link'>{props.title}</p>;
+}
+
+export default function App() {
+  const [currentProject] = useState('ห้างสรรพสินค้า ก');
+  const [fullname] = useState('Theresa Steward');
+  const [title] = useState('ผู้ดูแลระบบ');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-
-          aasdasds
-
-          adq222
-          <br/>
-          asdlkasdlka;ko
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ flexGrow: 1 }}>
+      <Router>
+        <Grid container>
+          <Grid item xs={2}>
+            <div style={{ backgroundColor: '#3C78D8', height: '100vh' }}>
+              <Grid container>
+                <Grid item xs={3} />
+                <Grid item xs={9}>
+                  <br /><br /><br /><br />
+                  <Link to="/" className="no-decoration"><MenuItem title='หน้าหลัก'/></Link>
+                  <Link to="/notification" className="no-decoration"><p className='link'>การแจ้งเตือน</p></Link>
+                  <br /><br />
+                  <Link to="/manage" className="no-decoration"><p className='link'>จัดการ</p></Link>
+                  <Link to="/overview" className="no-decoration"><p className='link'>ภาพรวม</p></Link>
+                  <Link to="/employee" className="no-decoration"><p className='link'>พนักงาน</p></Link>
+                  <Link to="/timetable" className="no-decoration"><p className='link'>ตารางเวลา</p></Link>
+                  <Link to="/wages" className="no-decoration"><p className='link'>คำนวณเงิน</p></Link>
+                </Grid>
+              </Grid>
+            </div>
+          </Grid>
+          <Grid item xs={10}>
+            <div className="header">
+              <Grid item xs={1} />
+              <Grid container>
+                <Grid item xs={4} className='align-vertical-center'>
+                  <h2 style={{ fontWeight: 500 }}>{currentProject}</h2>
+                </Grid>
+                <Grid item xs={4} />
+                <Grid item xs={3} className='align-vertical-center'>
+                  <Grid container spacing={2} >
+                    <Grid item>
+                      <div style={{ textAlign: 'right' }}>
+                        <span>{fullname}</span>
+                        <br />
+                        <span style={{ color: '#999999', fontSize: '0.8em' }}>{title}</span>
+                      </div>
+                    </Grid>
+                    <Grid item className='align-vertical-center'>
+                      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                    </Grid>
+                    <Grid item className='align-vertical-center'>
+                      <NotificationsNone />
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </div>
+            <div>
+              <Switch>
+                <Route exact path="/" >
+                  <HomeView />
+                </Route>
+                <Route path="/notification">
+                  <NotificationView />
+                </Route>
+                <Route path="/manage">
+                  <ManageView />
+                </Route>
+                <Route path="/overview">
+                  <OverviewView />
+                </Route>
+                <Route path="/employee">
+                  <EmployeeView />
+                </Route>
+                <Route path="/timetable">
+                  <TimetableView />
+                </Route>
+                <Route path="/wages">
+                  <WagesView />
+                </Route>
+              </Switch>
+            </div>
+          </Grid>
+        </Grid>
+      </Router>
     </div>
   );
 }
-
-export default App;
---
+++ web/src/views/Views.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export const HomeView: React.FC = () => {
+    return (
+        <div>Home</div>
+    );
+}
+
+export const NotificationView: React.FC = () => {
+    return (
+        <div>Notification</div>
+    );
+}
+
+export const ManageView: React.FC = () => {
+    return (
+        <div>Manage</div>
+    );
+}
+
+export const OverviewView: React.FC = () => {
+    return (
+        <div>Overview</div>
+    );
+}
+
+export const EmployeeView: React.FC = () => {
+    return (
+        <div>Employee</div>
+    );
+}
+
+export const TimetableView: React.FC = () => {
+    return (
+        <div>Timetable</div>
+    );
+}
+
+export const WagesView: React.FC = () => {
+    return (
+        <div>Wages</div>
+    );
+}
