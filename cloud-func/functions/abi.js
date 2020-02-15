305a4393d47af4722a07f10b94c387bbec6713da
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2020-02-16 06:19:44 +0700

    add functions for blockchain

--- cloud-func/functions/abi.js
+++ cloud-func/functions/abi.js
@@ -47,6 +47,314 @@ const factoryAbi = [
   }
 ];
 
+const requestFactoryAbi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_currentRequest",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_dateStart",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_dateEnd",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "_title",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_site",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_detail",
+				"type": "string"
+			}
+		],
+		"name": "addRequest",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getAllRequests",
+		"outputs": [
+			{
+				"internalType": "contract GuardRequest[]",
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+const requestAbi = [
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_requestor",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_currentRequest",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_dateStart",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_dateEnd",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "_title",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_site",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_detail",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "accept",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "cancel",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_number",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "title",
+				"type": "string"
+			}
+		],
+		"name": "contribute",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "currentContribute",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "currentRequest",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "dateEnd",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "dateStart",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "detail",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_number",
+				"type": "uint256"
+			}
+		],
+		"name": "removeContribution",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "requestor",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "requestorTitle",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "site",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "start",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "statusCode",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
 module.exports = {
-  factoryAbi
+  factoryAbi,
+  requestFactoryAbi,
+  requestAbi
 };
--- cloud-func/functions/index.js
+++ cloud-func/functions/index.js
@@ -114,7 +114,7 @@ exports.get_txid = functions.https.onRequest(async (req, res) => {
                   8,
                 10
               )
-            ),
+            ).toDateString(),
             stop: snapShot.docs[0].data().timeScanned
           }
         });
@@ -383,6 +383,250 @@ exports.get_tx = functions.https.onRequest(async (req, res) => {
         start_time: start,
         stop_time: stop,
         sender_address: sender
+      }
+    });
+  } else {
+    return res.status(404).json({
+      status: "invalid methods",
+      message: "not found"
+    });
+  }
+});
+
+exports.request_guard = functions.https.onRequest(async (req, res) => {
+  if (req.method.toUpperCase() === "POST") {
+    const { current_request_number, date_start, date_end, org_title, site, detail } = req.body;
+
+    const Web3 = require("web3");
+    const web3 = new Web3();
+    const { requestFactoryAbi } = require("./abi");
+
+    const HDWalletProvider = require("truffle-hdwallet-provider");
+    const provider = new HDWalletProvider(
+      "fee car zero banner tornado resist machine lend narrow measure later client",
+      "https://rpc.tch.in.th",
+      1
+    );
+    web3.setProvider(provider);
+
+    const factoryAddress = "0xb97534147eb792ed7ea9cb71c3e0520daa21bd99";
+
+    const factory = await new web3.eth.Contract(requestFactoryAbi, factoryAddress);
+    const accounts = await web3.eth.getAccounts();
+
+    const address = await factory.methods
+      .addRequest(current_request_number, date_start, date_end, org_title, site, detail)
+      .send({
+        from: accounts[0],
+        gas: "1000000"
+      });
+
+    return res.status(202).json({
+      status: "the transaction was immuted to the blockchain",
+      message: "added successfully",
+      data: {
+        address: address
+      }
+    });
+  } else {
+    return res.status(404).json({
+      status: "invalid methods",
+      message: "not found"
+    });
+  }
+});
+
+exports.start_request = functions.https.onRequest(async (req, res) => {
+  if (req.method.toUpperCase() === "POST") {
+    const { contract_address } = req.data
+    
+    const Web3 = require("web3");
+    const web3 = new Web3();
+    const { requestAbi } = require("./abi");
+
+    const HDWalletProvider = require("truffle-hdwallet-provider");
+    const provider = new HDWalletProvider(
+      "fee car zero banner tornado resist machine lend narrow measure later client",
+      "https://rpc.tch.in.th",
+      1
+    );
+    web3.setProvider(provider);
+
+    const request = await new web3.eth.Contract(requestAbi, contract_address);
+    const accounts = await web3.eth.getAccounts();
+
+    const address = await request.methods
+      .start()
+      .send({
+        from: accounts[0],
+        gas: "1000000"
+      });
+
+    return res.status(202).json({
+      status: "the transaction was immuted to the blockchain",
+      message: "contribution has started",
+      data: {
+        address: address
+      }
+    });
+  } else {
+    return res.status(404).json({
+      status: "invalid methods",
+      message: "not found"
+    });
+  }
+});
+
+exports.accept_request = functions.https.onRequest(async (req, res) => {
+  if (req.method.toUpperCase() === "POST") {
+    const { contract_address } = req.data
+    
+    const Web3 = require("web3");
+    const web3 = new Web3();
+    const { requestAbi } = require("./abi");
+
+    const HDWalletProvider = require("truffle-hdwallet-provider");
+    const provider = new HDWalletProvider(
+      "fee car zero banner tornado resist machine lend narrow measure later client",
+      "https://rpc.tch.in.th",
+      1
+    );
+    web3.setProvider(provider);
+
+    const request = await new web3.eth.Contract(requestAbi, contract_address);
+    const accounts = await web3.eth.getAccounts();
+
+    const address = await request.methods
+      .accept()
+      .send({
+        from: accounts[0],
+        gas: "1000000"
+      });
+
+    return res.status(202).json({
+      status: "the transaction was immuted to the blockchain",
+      message: "contribution has started",
+      data: {
+        address: address
+      }
+    });
+  } else {
+    return res.status(404).json({
+      status: "invalid methods",
+      message: "not found"
+    });
+  }
+});
+
+exports.cancel_request = functions.https.onRequest(async (req, res) => {
+  if (req.method.toUpperCase() === "POST") {
+    const { contract_address } = req.data
+    
+    const Web3 = require("web3");
+    const web3 = new Web3();
+    const { requestAbi } = require("./abi");
+
+    const HDWalletProvider = require("truffle-hdwallet-provider");
+    const provider = new HDWalletProvider(
+      "fee car zero banner tornado resist machine lend narrow measure later client",
+      "https://rpc.tch.in.th",
+      1
+    );
+    web3.setProvider(provider);
+
+    const request = await new web3.eth.Contract(requestAbi, contract_address);
+    const accounts = await web3.eth.getAccounts();
+
+    const address = await request.methods
+      .cancel()
+      .send({
+        from: accounts[0],
+        gas: "1000000"
+      });
+
+    return res.status(202).json({
+      status: "the transaction was immuted to the blockchain",
+      message: "contribution has started",
+      data: {
+        address: address
+      }
+    });
+  } else {
+    return res.status(404).json({
+      status: "invalid methods",
+      message: "not found"
+    });
+  }
+});
+
+exports.contribute = functions.https.onRequest(async (req, res) => {
+  if (req.method.toUpperCase() === "POST") {
+    const { contract_address, number, title } = req.data
+    
+    const Web3 = require("web3");
+    const web3 = new Web3();
+    const { requestAbi } = require("./abi");
+
+    const HDWalletProvider = require("truffle-hdwallet-provider");
+    const provider = new HDWalletProvider(
+      "fee car zero banner tornado resist machine lend narrow measure later client",
+      "https://rpc.tch.in.th",
+      1
+    );
+    web3.setProvider(provider);
+
+    const request = await new web3.eth.Contract(requestAbi, contract_address);
+    const accounts = await web3.eth.getAccounts();
+
+    const address = await request.methods
+      .contribute(number, title)
+      .send({
+        from: accounts[0],
+        gas: "1000000"
+      });
+
+    return res.status(202).json({
+      status: "the transaction was immuted to the blockchain",
+      message: "contribution has started",
+      data: {
+        address: address
+      }
+    });
+  } else {
+    return res.status(404).json({
+      status: "invalid methods",
+      message: "not found"
+    });
+  }
+});
+
+exports.check_request_status = functions.https.onRequest(async (req, res) => {
+  if (req.method.toUpperCase() === "GET") {
+    const { contract_address } = req.query
+    
+    const Web3 = require("web3");
+    const web3 = new Web3();
+    const { requestAbi } = require("./abi");
+
+    const HDWalletProvider = require("truffle-hdwallet-provider");
+    const provider = new HDWalletProvider(
+      "fee car zero banner tornado resist machine lend narrow measure later client",
+      "https://rpc.tch.in.th",
+      1
+    );
+    web3.setProvider(provider);
+
+    const request = await new web3.eth.Contract(requestAbi, contract_address);
+
+    const code = await request.methods
+      .statusCode()
+      .call();
+
+    return res.status(202).json({
+      status: "the transaction was retreived from the blockchain",
+      message: "checked!",
+      data: {
+        statusCode: code
       }
     });
   } else {
