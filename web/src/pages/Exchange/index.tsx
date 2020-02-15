745fa72e41f7ae71b558a85b1e0448ac53ff31e1
toey <anirut.workspace@gmail.com>
2020-02-16 06:42:26 +0700

    :zap: :zap: add nav action

---
+++ web/src/pages/Exchange/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ExchangePage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ExchangePage
--- web/src/pages/Home/components/card-home.tsx
+++ web/src/pages/Home/components/card-home.tsx
@@ -1,33 +1,36 @@
 import React, { Component } from "react";
 import "./card-home-style.scss";
-import { COLOR_PRIMARY } from "../../../constants";
+import { useHistory } from "react-router-dom";
 
-class CardHomeComponent extends Component<any> {
-  
-  render () {
-    return (
-      <div className="card-home">
-        <div className='left'>
-          <div>1</div>
+const CardHomeComponent = () => {
+  const history = useHistory();
+
+  return (
+    <div
+      className="card-home"
+      style={{
+        cursor: "pointer"
+      }}
+      onClick={() => {
+        history.push("/project");
+      }}
+    >
+      <div className="left">
+        <div>1</div>
+      </div>
+      <div className="right">
+        ห้างสรรพสินค้า ก
+        <div className="box">
+          ที่อยู่
+          <span>987 ถนนพระราม 1 แขวงปทุมวัน เขตปทุมวัน กทม</span>
         </div>
-        <div className='right'>
-          ห้างสรรพสินค้า ก
-          <div className='box'>
-            ที่อยู่
-            <span>
-              987 ถนนพระราม 1 แขวงปทุมวัน เขตปทุมวัน กทม
-            </span>
-          </div>
-          <div className='box'>
-            จำนวน
-            <span>
-              9 คน
-            </span>
-          </div>
+        <div className="box">
+          จำนวน
+          <span>9 คน</span>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default CardHomeComponent;
