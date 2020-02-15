e5b4495fbce4658045ffddf1de08a7df511aed55
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2020-02-15 17:10:32 +0700

    update menubar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
-  useRouteMatch,
-  useLocation
+  useRouteMatch
 } from 'react-router-dom';
 import './App.css';
 
@@ -14,12 +13,17 @@ import NotificationsNone from '@material-ui/icons/NotificationsNone';
 
 import { HomeView, NotificationView, ManageView, OverviewView, EmployeeView, TimetableView, WagesView } from './views/Views';
 
-const MenuItem = (props : any) => {
-  return <p className='link'>{props.title}</p>;
+const MenuItem = (props: any) => {
+  let match: any = useRouteMatch({
+    path: props.path,
+    strict: true,
+    sensitive: true
+  });
+  return match ? <p className='link-selected'>{props.title}</p> : <p className='link'>{props.title}</p>;
 }
 
 export default function App() {
-  const [currentProject] = useState('ห้างสรรพสินค้า ก');
+  const [currentTitle] = useState('ห้างสรรพสินค้า ก');
   const [fullname] = useState('Theresa Steward');
   const [title] = useState('ผู้ดูแลระบบ');
 
@@ -30,17 +34,30 @@ export default function App() {
           <Grid item xs={2}>
             <div style={{ backgroundColor: '#3C78D8', height: '100vh' }}>
               <Grid container>
-                <Grid item xs={3} />
-                <Grid item xs={9}>
+                <Grid item xs={2} />
+                <Grid item xs={10}>
                   <br /><br /><br /><br />
-                  <Link to="/" className="no-decoration"><MenuItem title='หน้าหลัก'/></Link>
-                  <Link to="/notification" className="no-decoration"><p className='link'>การแจ้งเตือน</p></Link>
+                  <Link to="/" className="no-decoration"><MenuItem title='หน้าหลัก' /></Link>
+                  <Link to="/notification" className="no-decoration"><MenuItem title='การแจ้งเตือน' path='/notification' /></Link>
                   <br /><br />
-                  <Link to="/manage" className="no-decoration"><p className='link'>จัดการ</p></Link>
-                  <Link to="/overview" className="no-decoration"><p className='link'>ภาพรวม</p></Link>
-                  <Link to="/employee" className="no-decoration"><p className='link'>พนักงาน</p></Link>
-                  <Link to="/timetable" className="no-decoration"><p className='link'>ตารางเวลา</p></Link>
-                  <Link to="/wages" className="no-decoration"><p className='link'>คำนวณเงิน</p></Link>
+                  <Link to="/manage" className="no-decoration"><MenuItem title='จัดการ' path='/manage' /></Link>
+                  <Link to="/overview" className="no-decoration"><MenuItem title='ภาพรวม' path='/overview' /></Link>
+                  <Link to="/employee" className="no-decoration"><MenuItem title='พนักงาน' path='/employee' /></Link>
+                  <Link to="/timetable" className="no-decoration"><MenuItem title='ตารางเวลา' path='/timetable' /></Link>
+                  <Link to="/wages" className="no-decoration"><MenuItem title='คำนวณเงิน' path='/wages' /></Link>
+                </Grid>
+              </Grid>
+              <br />
+              <Grid container spacing={2} style={{ display: 'flex', justifyContent: 'center', color: 'white' }} >
+                <Grid item className='align-vertical-center'>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                </Grid>
+                <Grid item>
+                  <div style={{ textAlign: 'left' }}>
+                    <span>{fullname}</span>
+                    <br />
+                    <span style={{ fontSize: '0.8em' }}>{title}</span>
+                  </div>
                 </Grid>
               </Grid>
             </div>
@@ -50,7 +67,7 @@ export default function App() {
               <Grid item xs={1} />
               <Grid container>
                 <Grid item xs={4} className='align-vertical-center'>
-                  <h2 style={{ fontWeight: 500 }}>{currentProject}</h2>
+                  <h2>{currentTitle}</h2>
                 </Grid>
                 <Grid item xs={4} />
                 <Grid item xs={3} className='align-vertical-center'>
